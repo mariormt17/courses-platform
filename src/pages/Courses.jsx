2288c880a9815f22cb23f0a846771b9e29c11494
Mario Mancilla <mariormt17@gmail.com>
2021-08-15 14:56:14 -0500

    Add the banners into the specialties and courses pages

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Banner from '../components/Banner'
 
 function Courses() {
   return (
-    <div className="ed-grid">
-      <h1>Cursos</h1>
-    </div>
+    <Banner
+      color="dark-color"
+      image={{
+        url: "https://www.cwjobs.co.uk/advice/wp-content/uploads/programmer-job-description-1024x576.jpg",
+        alternative: "banner-courses"
+      }}
+      title="Cursos"
+      subtitle="Comienza desde cero hoy mismo en tu camino a dominar la tecnología"
+    />
   )
 }
 
--- src/pages/Specialties.jsx
+++ src/pages/Specialties.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Banner from '../components/Banner'
 
 function Specialties() {
   return (
-    <div className="ed-grid">
-      <h1>Especialidades</h1>
-    </div>
+    <Banner
+      color="second-color"
+      image={{
+        url: "https://media.istockphoto.com/photos/business-people-working-in-the-office-picture-id1085389362?k=6&m=1085389362&s=612x612&w=0&h=Bnzvk6HStsJ22Vi58PBFV5xlJUD3Wa9MB-C7Jwkt61U=",
+        alternative: "banner-specialties"
+      }}
+      title="Especialidades"
+      subtitle="Domina la tecnología con las rutas de aprendizaje que te ofrecemos"
+    />
   )
 }
 
