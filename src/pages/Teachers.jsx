29d8769e4252395f87eeeb5581f05ed95402dfd7
Mario Mancilla <mariormt17@gmail.com>
2021-08-15 14:42:37 -0500

    Create the teachers grid with the data obtained from the api through redux

--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -1,18 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Banner from '../components/Banner'
+import store from '../redux/store'
+import { getAllTeachers } from '../redux/actionCreators'
+import { connect } from 'react-redux'
 
-function Teachers() {
+function Teachers({ match, teachers }) {
+  useEffect(() => {
+    store.dispatch(getAllTeachers());
+  }, [match])
+
   return (
-    <Banner
-      color="primary-color"
-      image={{
-        url: "https://www.ucn.edu.co/apoyo-inclusion/PublishingImages/banner_apoyoinclusion-equipo-trabajo.jpg",
-        alternative: "banner-teachers"
-      }}
-      title="Nuestros profesores"
-      subtitle="Este plantel docente está altamente calificado para guiarte en tu camino del aprendizaje."
-    />
+    <>
+      <Banner
+        color="primary-color"
+        image={{
+          url: "https://www.ucn.edu.co/apoyo-inclusion/PublishingImages/banner_apoyoinclusion-equipo-trabajo.jpg",
+          alternative: "banner-teachers"
+        }}
+        title="Nuestros profesores"
+        subtitle="Este plantel docente está altamente calificado para guiarte en tu camino del aprendizaje."
+      />
+      {teachers &&
+        <main className="ed-grid m-grid-3 lg-grid-4 row-gap">
+          {
+            teachers.map(teacher => {
+              return (
+                <article key={teacher.id}>
+                  <div className="s-px-4">
+                    <div className="img-container circle s-mb-2">
+                      <img src={teacher.avatar} alt={teacher.name} />
+                    </div>
+                  </div>
+                  <div className="s-center">
+                    <p className="t3 s-mb-1">{teacher.name}</p>
+                    <p>{teacher.country}</p>
+                  </div>
+                </article>
+              )
+            })
+          }
+        </main>
+      }
+    </>
   )
 }
 
-export default Teachers
+function mapStateToProps(state) {
+  return (
+    {
+      teachers: state.teacherReducer.teachers
+    }
+  )
+}
+
+export default connect(mapStateToProps, {})(Teachers)
